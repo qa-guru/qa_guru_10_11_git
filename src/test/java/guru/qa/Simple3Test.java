7c398ed1c3c063d189ee741eefd054ca11607e34
Dmitrii Tuchs <dtuchs@gmail.com>
2022-01-28 20:54:20 +0300

    Simple3Test.java

---
+++ src/test/java/guru/qa/Simple3Test.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Simple3Test {
+
+    @Test
+    void assertTrueTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
