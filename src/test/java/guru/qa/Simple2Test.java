a9b0cbed2009d8ecd79e0e48d2fd3af4f846c45d
Dmitrii Tuchs <d.tuchs@propellerads.net>
2022-01-28 20:59:21 +0300

    Added first 3 tests (#1)
    * Added first 3 tests

---
+++ src/test/java/guru/qa/Simple2Test.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Simple2Test {
+
+    @Test
+    void assertTrueTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
