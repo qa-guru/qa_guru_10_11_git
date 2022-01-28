92d54270dd045a3b83d675356b6c8211f10a281c
Dmitrii Tuchs <dtuchs@gmail.com>
2022-01-28 20:51:40 +0300

    Added assertTrue test

---
+++ src/test/java/guru/qa/SimpleTest.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void assertTrueTest() {
+        Assertions.assertTrue(3 > 2);
+    }
+}
